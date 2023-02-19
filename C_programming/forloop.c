6d2728182015f4867efa97f48cef966f61a8c0af
ConquerorCletus <smartchibuike2019@gmail.com>
2023-02-19 13:50:04 -0800

    for loop example

---
+++ C_programming/forloop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - this program is making use of for loops
+ * Return: 0 after success
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+		printf("%d \n", a);
+	return (0);
+}
