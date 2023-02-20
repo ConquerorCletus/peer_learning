97cfecdea59c3f013c5e74d4de11a5a09635b78f
Hintrovert <chymexky@gmail.com>
2023-02-20 08:22:48 -0800

    Making my first commit

---
+++ C_programming/tryout.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - write a code that inputs the ASCII value of 'a'
+ * return 0 on success
+ */
+int main(void) 
+{
+	int a = 'a';
+	printf("%d\n", a);
+	return (0);
+}
