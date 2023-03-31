b2b7baf21b6f2924cf7de6e46d62190ff3e2a850
ConquerorCletus <smartchibuike2019@gmail.com>
2023-03-31 16:15:55 +0100

    linked list

---
+++ C_programming/linked_list/README.md
@@ -0,0 +1,13 @@
+# Linked list.
+Linked list is  a representation multiples datum in a list with help of a pointer to the next data.
+
+## factors that makes up a linked list.
+- pointer
+- structure or typedef
+- memory allocation.
+
+## Visual representation of Linked list.
+( HEAD)                         (TAIL)
+|data1|ptr|-->|data2|ptr|-->|data3|ptr|
+        |              |            |
+        data2   >>  >> data3       NULL
---
+++ C_programming/linked_list/link.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+	int data;
+	struct node *link;
+};
+/*
+ * |data|link = address of the next data
+ *   head
+ *
+ * */
+
+int main()
+{
+	/**
+	 *
+	 * |data1|ptr2 >>> |data2|ptr3
+	 * data|ptr>>>>next data
+	 * head 
+	 *
+	 * 45, 80, 4
+	 */
+	int count = 0;
+	
+	struct node *head = NULL;
+	head = malloc(sizeof(struct node));
+	head -> data = 45;
+	head -> link = NULL;
+
+	struct node *new;
+	new = malloc(sizeof(struct node));
+	new -> data = 80;
+	new -> link = NULL;
+	head -> link = new;
+
+	new = malloc(sizeof(struct node));
+	new -> data = 4;
+	new -> link = NULL;
+	head -> link -> link = new;
+
+	struct node *ptr = NULL;
+	ptr = head;
+	while (ptr != NULL)
+	{
+		count++;
+		printf("%d : %d\n", ptr -> data, count);
+		ptr = ptr -> link;
+	}
+
+	return (0);
+
+}
+
+
+
